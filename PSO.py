8b6ba962dbbfcb37313f1a4ec42b1678b4feb4ce
Matthew_Huang <a26833765@gmail.com>
2024-09-20 17:43:28 +0800

    feat: tmp

--- PSO.py
+++ PSO.py
@@ -1,74 +1,103 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jun  3 16:13:49 2020
+import time
+from typing import Callable
 
-@author: e10832
-"""
 import numpy as np
-import matplotlib.pyplot as plt
-np.random.seed(42)
+import plotly.graph_objects as go
 
-class PSO():
-    def __init__(self, fitness, D=30, P=20, G=500, ub=1, lb=0,
-                 w_max=0.9, w_min=0.4, c1=2.0, c2=2.0, k=0.2):
-        self.fitness = fitness
-        self.D = D
-        self.P = P
-        self.G = G
-        self.ub = ub*np.ones([self.P, self.D])
-        self.lb = lb*np.ones([self.P, self.D])
+
+class pso:
+    def __init__(
+        self,
+        max_iter: int,
+        pop_size: int,
+        k: float,
+        w_max: float,
+        w_min: float,
+        c1: float,
+        c2: float,
+        lb: list,
+        ub: list,
+        benchmark: Callable,
+        name: str = "",
+    ) -> None:
+        self.max_iter = max_iter
+        self.pop_size = pop_size
+        self.v_max = k * (ub - lb)
         self.w_max = w_max
         self.w_min = w_min
         self.c1 = c1
         self.c2 = c2
-        self.k = k
-        self.v_max = self.k*(self.ub-self.lb)
-        
-        self.pbest_X = np.zeros([self.P, self.D])
-        self.pbest_F = np.zeros([self.P]) + np.inf
-        self.gbest_X = np.zeros([self.D])
-        self.gbest_F = np.inf
-        self.loss_curve = np.zeros(self.G)
+        self.lb = lb
+        self.ub = ub
+        self.dim = len(lb)
+        self.name = name
+        self.benchmark = benchmark
 
-    def opt(self):
+        self.cost = 0
+        self.curve = list()
+        self.gbest_f = np.inf
+        self.gbest_x = np.zeros(self.dim)
+        self.pbest_F = np.full(self.dim, np.inf)
+        self.pbest_X = np.zeros([self.pop_size, self.dim])
+
+    def run(self):
+        st = time.time()
+
         # 初始化
-        self.X = np.random.uniform(low=self.lb, high=self.ub, size=[self.P, self.D])
-        self.V = np.zeros([self.P, self.D])
-        
+        X = np.random.uniform(low=self.lb, high=self.ub, size=[self.pop_size, self.dim])
+        V = np.zeros([self.pop_size, self.dim])
+
         # 迭代
-        for g in range(self.G):
+        for _iter in range(self.max_iter):
             # 適應值計算
-            F = self.fitness(self.X)
-            
+            F = self.benchmark(X)
+
             # 更新最佳解
             mask = F < self.pbest_F
-            self.pbest_X[mask] = self.X[mask].copy()
+            self.pbest_X[mask] = X[mask].copy()
             self.pbest_F[mask] = F[mask].copy()
-            
-            if np.min(F) < self.gbest_F:
-                idx = F.argmin()
-                self.gbest_X = self.X[idx].copy()
-                self.gbest_F = F.min()
-            
-            # 收斂曲線
-            self.loss_curve[g] = self.gbest_F
-            
-            # 更新
-            r1 = np.random.uniform(size=[self.P, self.D])
-            r2 = np.random.uniform(size=[self.P, self.D])
-            w = self.w_max - (self.w_max-self.w_min)*(g/self.G)
-            
-            self.V = w * self.V + self.c1 * (self.pbest_X - self.X) * r1 \
-                                + self.c2 * (self.gbest_X - self.X) * r2 # 更新V
-            self.V = np.clip(self.V, -self.v_max, self.v_max) # 邊界處理
-            
-            self.X = self.X + self.V # 更新X
-            self.X = np.clip(self.X, self.lb, self.ub) # 邊界處理
 
-    def plot_curve(self):
-        plt.figure()
-        plt.title('loss curve ['+str(round(self.gBest_curve[-1], 3))+']')
-        plt.plot(self.gBest_curve, label='loss')
-        plt.grid()
-        plt.legend()
-        plt.show()
+            if self.pbest_F.min() < self.gbest_f:
+                idx = self.pbest_F.argmin()
+                self.gbest_x = self.pbest_X[idx].copy()
+                self.gbest_f = self.pbest_F.min()
+
+            # 收斂曲線
+            self.curve.append(self.gbest_f)
+
+            # 更新
+            R1 = np.random.uniform(size=[self.pop_size, self.dim])
+            R2 = np.random.uniform(size=[self.pop_size, self.dim])
+            w = self.w_max - (self.w_max - self.w_min) * (_iter / self.max_iter)
+
+            V = (
+                w * V
+                + self.c1 * (self.pbest_X - X) * R1
+                + self.c2 * (self.gbest_x - X) * R2
+            )  # 更新V
+            V = np.clip(V, -self.v_max, self.v_max)  # 邊界處理
+
+            X += V  # 更新 X
+            X = np.clip(X, self.lb, self.ub)  # 邊界處理
+
+        # 總計算時間
+        ed = time.time()
+        self.cost = round(ed - st, 2)
+
+    def plot(self):
+        if self.curve:
+            # 創建 Figure
+            fig = go.Figure()
+
+            # 添加數據列到 Figure
+            fig.add_trace(go.Scatter(y=self.curve, mode="lines+markers"))
+
+            # 設置圖表標題及坐標軸標籤
+            fig.update_layout(
+                title=f"{self.name}(best fitness: {min(self.curve):.2f}, cost: {self.cost} sec)",
+                xaxis_title="Iteration",
+                yaxis_title="Fitness",
+            )
+
+            # 顯示圖表
+            fig.show()
